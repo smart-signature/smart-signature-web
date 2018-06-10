575c597f7ec462952c78bc8df34d295ba51427d1
KJlmfe <kjlmfe@gmail.com>
2018-06-10 18:17:05 +0800

    feat: add mock api(getUserById,createLike)

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+import user from './user';
+import like from './like';
+
+export default {
+  user,
+  like,
+};
---
+++ src/api/like.js
@@ -0,0 +1,7 @@
+export default {
+  async createLike({ userId, itemId = 0, value, msg = '' }) {
+    await Promise.delay(2000);
+    const tx = '0x82e93f98ebafe0dc35263783e222b97fbe2f434dbedccf028059a35112ec2f4a';
+    return tx;
+  },
+};
